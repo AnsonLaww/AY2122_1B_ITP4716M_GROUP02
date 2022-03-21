ecbd17dcea32d40d8593226d88ea02f1b6c37321
lichunhong426 <210108464@stu.vtc.edu.hk>
2022-03-22 00:53:17 +0800

    Add Main Menu and health bar UI

---
+++ Project_Group02/Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public int MaxHealth = 100;
+    public int CurrentHealth;
+
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentHealth = MaxHealth;
+        healthBar.SetMaxHealth(MaxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(5);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        CurrentHealth -= damage;
+
+        healthBar.SetHealth(CurrentHealth);
+    }
+
+
+}
