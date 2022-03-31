58e7811b79a7375eba3022291f19f216a01e10da
lichunhong426 <210108464@stu.vtc.edu.hk>
2022-03-31 13:04:41 +0800

    Update Monster Spawn

--- Project_Group02/Assets/Scripts/Monsters/GenerateMonster.cs
+++ Project_Group02/Assets/Scripts/Monsters/GenerateMonster.cs
@@ -7,7 +7,7 @@ public class GenerateMonster : MonoBehaviour
     public GameObject monster;
     public int xPos;
     public int zPos;
-    public int monsterCount;
+    public int monsterCount = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -20,18 +20,21 @@ public class GenerateMonster : MonoBehaviour
     {
         while (monsterCount < 10)
         {
-            xPos = Random.Range(1, 12);
-            zPos = Random.Range(6, 19);
-            Instantiate(monster, new Vector3(xPos, 0, zPos), Quaternion.identity);
+            xPos = Random.Range(10, 120);
+            zPos = Random.Range(60, 190);
+
+
             yield return new WaitForSeconds(0.1f);
-            monsterCount += 1;
+
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && monsterCount < 10)
         {
+            Instantiate(monster, new Vector3(xPos, 0, zPos), Quaternion.identity);
+            monsterCount += 1;
             StartCoroutine(MonsterDrop());
         }
     }
