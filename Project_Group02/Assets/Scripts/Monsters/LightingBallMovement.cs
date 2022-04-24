6b009775630ee39e833a714b2eab38b96031ec00
lichunhong426 <210108464@stu.vtc.edu.hk>
2022-04-24 15:05:34 +0800

    Update Light Ball

--- Project_Group02/Assets/Scripts/Monsters/LightingBallMovement.cs
+++ Project_Group02/Assets/Scripts/Monsters/LightingBallMovement.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class LightingBallMovement : MonoBehaviour
 {
+    Animation ballAnim;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ballAnim = GetComponent<Animation>();
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    IEnumerator Counter()
+    {
+        yield return new WaitForSeconds(3);
+        ballAnim.Stop();
+    }
+
+
 }
